379ae119c1ee49f797a7721d4899d962624fb94d
Sunil Rohilla <sunilrohilla276@gmail.com>
2025-08-17 20:48:29 +0530

    added model pipeline

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import test_data_data_processing
 import data_processing_pipeline_train
+import model_pipeline
 
 
 def main():
     train_data=data_processing_pipeline_train.run()
     test_data=test_data_data_processing.run()
+    model_pipeline.run()
+
     
 
 
